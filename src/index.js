25cb5fe80af2086402f8641387dafeafc9a93647
Olga Polishchuk <olgapolichuk@ukr.net>
2023-02-21 19:56:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 1000;
 const URL = `https://restcountries.com/v3.1/name/`;
 const settingsSearchArr = [
-  '?fields=',
   'name',
   'capital',
   'population',
   'flags',
   'languages',
 ];
+
 
 const fetchCountriesInput = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
@@ -24,11 +24,11 @@ fetchCountriesInput.addEventListener(
 );
 
 function handleInputEvent(e) {
-  if (document.querySelector('.country-item')) removeCountriesList();
-  if (document.querySelector('.name-country')) removeCountryInfo();
+  removeCountriesList();
+  removeCountryInfo();
   let searchQuery = e.target.value.trim();
-  if (searchQuery.length === 0) return;
-  fetchCountries(URL + searchQuery + settingsSearchArr.join(','))
+  if (!searchQuery.length) return;
+  fetchCountries(URL + searchQuery + "?fields=" + settingsSearchArr.join(','))
     .then(handleResponse)
     .catch(error =>
       Notify.failure(`Oops, there is no country with that name! (${error})`)
@@ -40,12 +40,15 @@ function doCountriesListMarkup(countries) {
     .map(
       ({ flags, name }) => `<li class="country-item">
     <img class="flag-img" src="${flags.svg}" alt="${name.official} flag" width="50" height="33"/>
-    <p class="country-name" >${name.official}</p></li>`).join('');
+    <p class="country-name" >${name.official}</p></li>`
+    )
+    .join('');
   addListOfCountries(markup);
 }
 function doCountryInfoMarkup(countries) {
-  const markup = countries.map(
-    ({ flags, name, capital, population, languages }) =>
+  const { flags, name, capital, population, languages } = countries
+  
+    const markup = 
       `<div class="wrapper">
       <img class="flag-img" src="${flags.svg}" alt="${
         name.official
@@ -53,11 +56,10 @@ function doCountryInfoMarkup(countries) {
     <p class="name-country">${name.official}</p></div>
     <p class="capital-country"><span class="info">Capital:</span> ${capital}</p>
     <p class="population-country"><span class="info">Population:</span> ${population}</p>
-    <p class="languages-country"><span class="info">Languages:</span> ${Object.keys(languages).join(
-      ', '
-    )}</p>
+    <p class="languages-country"><span class="info">Languages:</span> ${Object.keys(
+      languages
+    ).join(', ')}</p>
     `
-  );
   addCountryInfo(markup);
 }
 
@@ -77,13 +79,15 @@ function removeCountryInfo() {
 }
 
 function handleResponse(countries) {
-    if (countries.length > 10) {
-      return Notify.info('Too many matches found. Please enter a more specific name.');
-    }
-    if (countries.length > 2 && countries.length < 10) {
-      doCountriesListMarkup(countries);
-    }
-    if (countries.length === 1) {
-      doCountryInfoMarkup(countries);
-    }
+  if (countries.length > 10) {
+    return Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
   }
+  if (countries.length > 2 && countries.length < 10) {
+    doCountriesListMarkup(countries);
+  }
+  if (countries.length === 1) {
+    doCountryInfoMarkup(countries[0]);
+  }
+}
